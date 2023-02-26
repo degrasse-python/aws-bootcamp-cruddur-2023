991db9d03c05e914575a7c86b40387ddf5836e65
Deon 'PrimeTime' Saunders <adsaunde@protonmail.com>
2023-02-26 11:27:20 -0500

    init

---
+++ env.sh
@@ -0,0 +1,5 @@
+export HONEYCOMB_API_KEY="RlgUH0qga965f9W2yXmaBD"
+export HONEYCOMB_SERVICE_NAME="Cruddur"
+export AWS_REGION="ca-central-1"
+gp env HONEYCOMB_API_KEY="RlgUH0qga965f9W2yXmaBD"
+gp env HONEYCOMB_SERVICE_NAME="Cruddur"
